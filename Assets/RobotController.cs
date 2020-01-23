a2f9d8afc0882b1e8ca1a386c816da30868d57e5
Žan Kavčič <zan.kavcicdev@gmail.com>
2020-01-23 22:00:56 +0100

    just for merge test

--- Assets/RobotController.cs
+++ Assets/RobotController.cs
@@ -7,23 +7,37 @@ public class RobotController : MonoBehaviour
     [SerializeField] Rigidbody leftWheel;
     [SerializeField] Rigidbody rightWheel;
     [SerializeField] Transform robotBody;
-    [SerializeField] float speed;
 
+    [SerializeField] float speed;
+    [SerializeField] float movingAngle = 20f;
     private void FixedUpdate()
     {
         float vert = Input.GetAxisRaw("Vertical");
-        float hor = Input.GetAxisRaw("Horizontal");
-        Debug.Log("vert: " + vert + " ,hor: " + hor);
 
-        float left = 0.5f * vert + ((hor == 1f) ? 0.5f * vert : 0f);
-        float right = 0.5f * vert + ((hor == -1f) ? 0.5f * vert : 0f);
+        float bodyAngle = GetBodyAngle();
 
+
+
+        //float hor = Input.GetAxisRaw("Horizontal");
+        Debug.Log("vert: " + vert);// + " ,hor: " + hor);
+
+        //float left = 0.5f * vert + ((hor == 1f) ? 0.5f * vert : 0f);
+        //float right = 0.5f * vert + ((hor == -1f) ? 0.5f * vert : 0f);
+        float left = vert * 0.5f;
+        float right = vert * 0.5f;
         Debug.Log("left: " + left + " ,right: " + right);
 
         leftWheel.angularVelocity = leftWheel.transform.right * left * speed;
         rightWheel.angularVelocity = rightWheel.transform.right * right * speed;
+    }
 
-        float bodyAngle = GetBodyAngle();
+    public float CalcDesiredAngle()
+    {
+        float input = Input.GetAxisRaw("Vertical");
+        if (input == 1f)
+            return movingAngle;
+        else if(I)
+
     }
 
     public float GetBodyAngle()
@@ -36,5 +50,11 @@ public class RobotController : MonoBehaviour
         if (Vector3.Dot(cross, transform.right) > 0f)
             return angle;
         return -angle;
+    }
+
+    public float CalcError(float angle,float optimalAngle)
+    {
+        float currError = optimalAngle - angle;
+        return currError;
     }
 }
