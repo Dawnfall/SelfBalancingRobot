d2d501fcb953a0da181b550f1f1564664dab685d
Žan Kavčič <zan@deovr.com>
2020-01-30 18:43:15 +0100

    camera added, torque instead of force

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float distance;
+
+    [SerializeField] RobotController Robot;
+
+    private void Update()
+    {
+        if (Robot == null)
+            return;
+
+        Vector3 leftWheelPos = Robot.LeftWheel.WheelRigidBody.transform.position;
+        Vector3 rightWheelPos = Robot.RightWheel.WheelRigidBody.transform.position;
+
+        Vector3 centerPoint = leftWheelPos + (rightWheelPos - leftWheelPos);
+
+        Vector3 camPos = centerPoint + Vector3.up * distance;
+
+        transform.position = camPos;
+
+
+    }
+}
